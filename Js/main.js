a37ce91becf60ad41621a05149ee54092a28b56e
Cleinz <decoder145@gmail.com>
2019-11-30 21:08:18 -0400

    adam

---
+++ Js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $('.slider__list').slick ({
+        arrows: true,
+        dots: false,
+        centerMode: true,
+        slidesToShow: 3,
+        prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+        nextArrow: '<button type="button" class="slick-next">Previous</button>'
+    });
+
+}); 
