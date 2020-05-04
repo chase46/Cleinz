0cc063e532d0e3f1da6385748d46c38998918dad
Cleinz <decoder145@gmail.com>
2020-05-04 11:20:54 -0400

    Added sticky header and fixed clippath bug

--- Js/main.js
+++ Js/main.js
@@ -2,11 +2,29 @@ $(document).ready(function() {
 
     $('.slider__list').slick ({
         arrows: true,
-        dots: false,
         centerMode: true,
         slidesToShow: 3,
         prevArrow: '<button type="button" class="slick-prev">Previous</button>',
-        nextArrow: '<button type="button" class="slick-next">Previous</button>'
+        nextArrow: '<button type="button" class="slick-next">Next</button>'
     });
 
+
 }); 
+
+
+const nav = document.querySelector('.nav');
+const topNav = nav.offsetTop;
+const header = document.querySelector('.header');
+
+function stickNav() {
+    if(window.scrollY >= topNav) {
+        header.style.paddingTop = nav.offsetHeight  +'px';
+        nav.classList.add('sticky')
+    } else {
+        header.style.paddingTop = 0;
+        nav.classList.remove('sticky')
+        
+    }
+}
+
+window.addEventListener('scroll', stickNav)
